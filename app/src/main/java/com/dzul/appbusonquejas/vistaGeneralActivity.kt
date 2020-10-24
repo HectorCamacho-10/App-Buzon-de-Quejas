29c213c538eb5dda8c2b36eb65b272140a5d9bc7
Jesus Geovany Dzul Canul <geovany.jg14@gmail.com>
2020-10-24 17:17:31 -0500

    Intefas

---
+++ app/src/main/java/com/dzul/appbusonquejas/vistaGeneralActivity.kt
@@ -0,0 +1,11 @@
+package com.dzul.appbusonquejas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class vistaGeneralActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vista_general)
+    }
+}
