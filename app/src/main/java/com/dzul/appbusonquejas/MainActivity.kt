9a0dda8d8c67f441ffd49b0b866d0dc6c942fa32
HectorCamacho-10 <hector.camchoreyes@itsva.edu.mx>
2020-11-03 02:57:17 -0600

    Login y Otras Caracteristicas

--- app/src/main/java/com/dzul/appbusonquejas/MainActivity.kt
+++ app/src/main/java/com/dzul/appbusonquejas/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.dzul.appbusonquejas
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme) //SplashSecreen
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Logeo para proceder a iniciar el otro activity -Esto es temporal solo para ver el funcinamieto de la itnerfass
+        log_in.setOnClickListener{
+            val iniciar = Intent (this, vistaGeneralActivity::class.java)
+            startActivity(iniciar)
+
+        }
+
+
     }
+
 }
+
+
+
+
