b6d14c5bb62caf6981a904e9f51280f6096f7f48
Dany Daniel Uch <43461141+DaniDaniel88@users.noreply.github.com>
2020-11-18 16:37:40 -0600

    nueva actulizacion
    Actulizacion de botones y colores

--- app/src/main/java/com/dzul/appbusonquejas/registroActivity.kt
+++ app/src/main/java/com/dzul/appbusonquejas/registroActivity.kt
@@ -1,11 +1,24 @@
 package com.dzul.appbusonquejas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_registro.*
+import kotlin.time.seconds
 
 class registroActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        button_atras_registro.setOnClickListener{
+            val iniciar = Intent (this, MainActivity::class.java)
+            startActivity(iniciar)
+        }
+
     }
 }
