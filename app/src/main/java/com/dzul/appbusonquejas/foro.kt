d3e11f350f56f7cbe9582bc013c9aab2c395fd88
HectorCamacho-10 <hector.camchoreyes@itsva.edu.mx>
2021-01-27 01:11:39 -0600

    Menu Inferior

--- app/src/main/java/com/dzul/appbusonquejas/foro.kt
+++ app/src/main/java/com/dzul/appbusonquejas/foro.kt
@@ -1,8 +1,9 @@
 package com.dzul.appbusonquejas
-
+import android.animation.Animator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.ActivityNavigator
 import kotlinx.android.synthetic.main.activity_foro.*
 import kotlinx.android.synthetic.main.activity_foro.Boton_Home
 import kotlinx.android.synthetic.main.activity_foro.boton_Foro
@@ -10,26 +11,26 @@ import kotlinx.android.synthetic.main.activity_nuevavistageneral.*
 import kotlinx.android.synthetic.main.activity_perfil.*
 
 class foro : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_foro)
+
+        ActivityNavigator.applyPopAnimationsToPendingTransition(this)
+
         btnRegresar.setOnClickListener {
             val iniciar = Intent (this, nuevavistageneral::class.java)
             startActivity(iniciar)
         }
 
 
-        //Por alguna razon no puedo ir de Foro a perfil Solo el error en foro
-        /*Boton_Perfil.setOnClickListener{
-            val iniciar3 = Intent(this, Perfil::class.java)
-            startActivity(iniciar3)
-        }*/
-
-
 
         boton_Foro.setOnClickListener{
             val iniciar2 = Intent(this, foro::class.java)
             startActivity(iniciar2)
+
+
         }
 
 
@@ -41,9 +42,11 @@ class foro : AppCompatActivity() {
        imageButton2.setOnClickListener{
            val iniciar3 = Intent( this, Perfil::class.java)
            startActivity(iniciar3)
+
+           //Animatoo.animateZoom(context);
 }
 
+        }
 
 
-    }
 }
